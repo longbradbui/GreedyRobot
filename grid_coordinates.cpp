a999b9ab561f8c1dc8b8696e4dba1b4d7e72af47
Long Bui <bhoanglong15020@gmail.com>
2024-01-24 23:27:03 -0800

    Implemented Grid Coordinates

--- grid_coordinates.cpp
+++ grid_coordinates.cpp
@@ -1,1 +1,33 @@
 #include "grid_coordinates.h"
+
+Coordinates::Coordinates()
+{
+	SetX(0);
+	SetY(0);
+}
+
+Coordinates::Coordinates(int x, int y)
+{
+	SetX(x);
+	SetY(y);
+}
+
+int Coordinates::x() const
+{
+	return x_;
+}
+
+int Coordinates::y() const
+{
+	return y_;
+}
+
+void Coordinates::SetX(int x)
+{
+	this->x_ = x;
+}
+
+void Coordinates::SetY(int y)
+{
+	this->y_ = y;
+}
--- grid_coordinates.h
+++ grid_coordinates.h
@@ -1,5 +1,18 @@
-#pragma once
+#ifndef GRID_COORDINATES_H_
+#define GRID_COORDINATES_H_
 class Coordinates
 {
+public:
+	Coordinates();
+	Coordinates(int x, int y);
+	int x() const;
+	int y() const;
+	void SetX(int x);
+	void SetY(int y);
+
+private:
+	int x_;
+	int y_;
 };
+#endif
 
