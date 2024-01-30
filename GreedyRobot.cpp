a68a39cce1748c1fb5bd12689bd2a4147c290b9a
Long Bui <bhoanglong15020@gmail.com>
2024-01-30 15:21:38 -0800

    Tested Before Implementation

--- GreedyRobot.cpp
+++ GreedyRobot.cpp
@@ -6,4 +6,14 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
+    Robot r(2,1,2,3,5);
+    cout << r.GetShortestPath() << endl;
+    cout << r.GetMaxDistance() << endl;
+    cout << r.GetTotalPaths() << endl;
+    cout << r.RobotX() << endl;
+    cout << r.RobotY() << endl;
+    cout << r.TreasureX() << endl;
+    cout << r.TreasureY() << endl;
+    cout << r.PathSoFar() << endl; // this is currently an empty string
+    return 0;
 }
