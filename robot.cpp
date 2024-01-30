c06ec949c20d6800d58e66917212a32508ef0b97
Long Bui <bhoanglong15020@gmail.com>
2024-01-30 15:11:41 -0800

    Updated Naming Guidelines

--- robot.cpp
+++ robot.cpp
@@ -10,6 +10,8 @@ Robot::Robot()
 	robot_location_.SetY(1);
 	treasure_location_.SetX(1);
 	treasure_location_.SetY(1);
+	num_paths_ = 0;
+	path_so_far_ = "";
 }
 
 Robot::Robot(int max_distance, int robot_x, int robot_y, int treasure_x, int treasure_y)
@@ -19,6 +21,8 @@ Robot::Robot(int max_distance, int robot_x, int robot_y, int treasure_x, int tre
 	robot_location_.SetY(robot_y);
 	treasure_location_.SetX(treasure_x);
 	treasure_location_.SetY(treasure_y);
+	num_paths_ = 0;
+	path_so_far_ = "";
 }
 
 int Robot::RobotX() const
@@ -54,4 +58,24 @@ int Robot::GetMaxDistance() const
 int Robot::GetShortestPath() const
 {
 	return (abs(RobotX() - TreasureX()) + abs(RobotY() - TreasureY()));
+}
+
+string Robot::PathSoFar() const
+{
+	return path_so_far_;
+}
+
+vector<string> Robot::RobotPath() const
+{
+	return robot_path_;
+}
+
+void Robot::SetPathSoFar(string& new_move)
+{
+	path_so_far_ += new_move;
+}
+
+void Robot::SetRobotPath(string& new_path)
+{
+	robot_path_.push_back(new_path);
 }
--- robot.h
+++ robot.h
@@ -3,6 +3,7 @@
 #include "grid_coordinates.h"
 #include <vector>
 #include <string>
+using namespace std;
 
 class Robot
 {
@@ -16,13 +17,17 @@ public:
 	int GetTotalPaths() const;
 	int GetMaxDistance() const;
 	int GetShortestPath() const;
+	string PathSoFar() const;
+	vector<string> RobotPath() const;
+	void SetPathSoFar(string& new_move);
+	void SetRobotPath(string& new_path);
 
 private:
 	int max_distance_;
 	int num_paths_;
 	int shortest_path_;
 	string path_so_far_;
-	vector<string> robot_path;
+	vector<string> robot_path_;
 	Coordinates robot_location_;
 	Coordinates treasure_location_;
 };
